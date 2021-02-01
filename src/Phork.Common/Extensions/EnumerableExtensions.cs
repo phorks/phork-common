02b601057b661663a8f1602b63d83d59fc426220
Pharaz Fadaei <pharazfadaei@outlook.com>
2021-02-01 18:40:01 +0330

    Add GetSequenceHashCode with custom IEqualityComparer

--- src/Phork.Common/Extensions/EnumerableExtensions.cs
+++ src/Phork.Common/Extensions/EnumerableExtensions.cs
@@ -14,10 +14,27 @@ namespace System.Collections.Generic
 
         public static int GetSequenceHashCode<T>(this IEnumerable<T> enumerable)
         {
+            Guard.ArgumentNotNull(enumerable, nameof(enumerable));
+
             HashCode hashCode = new HashCode();
 
             foreach (var item in enumerable)
                 hashCode.Add(item);
+
+            return hashCode.ToHashCode();
+        }
+
+        public static int GetSequenceHashCode<T>(
+            this IEnumerable<T> enumerable,
+            IEqualityComparer<T> equalityComparer)
+        {
+            Guard.ArgumentNotNull(enumerable, nameof(enumerable));
+            Guard.ArgumentNotNull(equalityComparer, nameof(equalityComparer));
+
+            HashCode hashCode = new HashCode();
+
+            foreach (var item in enumerable)
+                hashCode.Add(equalityComparer.GetHashCode(item));
 
             return hashCode.ToHashCode();
         }
